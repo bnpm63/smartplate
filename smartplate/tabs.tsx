402ff23d89daa54660e9ff186824722c7e4b7ca5
bnpm63 <150587691+bnpm63@users.noreply.github.com>
2024-02-19 16:20:51 -0800

    Revert "changed imports"
    This reverts commit b5b955a3b515548386d0a3c8ae89332feab1128b, reversing
    changes made to a50e1db008bc7fd319a2a96bd4f76f89ac15ffa7.

--- smartplate/tabs.tsx
+++ smartplate/tabs.tsx
@@ -24,8 +24,8 @@ const PlateTab = () => {
 				options={{ title: 'Welcome' }}
 			/>
 			<Tab.Screen
-				name="Food"
-				component={Food}
+				name="Login"
+				component={Login}
 				options={{ title: 'Food' }}
 			/>
 			<Tab.Screen
@@ -34,9 +34,9 @@ const PlateTab = () => {
 				options={{ title: 'Friends' }}
 			/>
 			<Tab.Screen
-				name="Login"
-				component={Login}
-				options={{ title: 'Login' }}
+				name="Food"
+				component={Food}
+				options={{ title: 'Food' }}
 			/>
 		</Tab.Navigator>
 	);
